00512bb208d5a0d9fc323dd50fa42337e6ada35a
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-08-29 11:10:20 -0500

    working on controllers

--- client/app.js
+++ client/app.js
@@ -1,22 +1,41 @@
 var DrinkApp = angular.module("DrinkApp", ['ngRoute'])
 .config(function ($routeProvider) {
     $routeProvider
+        .when("/", {
+            templateUrl: "./views/login.html"
+        })
         .when("/home", {
             templateUrl: "./views/home.html"
         })
         .when("/main", {
             templateUrl: "./views/main.html"
         })
-        .when("/profile", {
+        .when("/user", {
             templateUrl: "./views/user.html"
         })
         .when("/achievements", {
             templateUrl: "./views/achievements.html"
         })
-        // .when("/invoice", {
-        //     templateUrl: "./views/invoice.html"
-        // })
-        // .when("/one/:id", {
-        //     templateUrl: "./views/single.html"
-        // });
+        .when("/drink/:id", {
+            templateUrl: "./views/singledrink.html"
+        })
+        .when("/drinks", {
+            templateUrl: "./views/drinks.html"
+        });
 })
+.run( function($rootScope){
+    $rootScope.drinks=[]
+    console.log(addb)
+    addb.init({
+        appId: 14561
+    });
+})
+
+DrinkApp.controller('DrinksListController', function($http, $scope, $routeParams, $location, $rootScope){
+addb.drinks().loadSet(function(query) { });
+addb.drinks('eg').skip(0).take(25).loadSet(function(query) { 
+    $scope.drinks = query.result
+    console.log($scope.drinks)
+    
+});
+}) 
