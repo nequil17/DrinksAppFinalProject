7071d1d39b5e720cc1cb29272e6d64360d67f8a6
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-08-29 14:48:00 -0500

    videos imbeded

--- client/app.js
+++ client/app.js
@@ -1,5 +1,14 @@
+// var next= next +=10
+// var previous= previous -+10
+
 var DrinkApp = angular.module("DrinkApp", ['ngRoute'])
-    .config(function ($routeProvider) {
+.config(function($sceProvider) {
+    // Completely disable SCE.  For demonstration purposes only!
+    // Do not use in new projects or libraries.
+    $sceProvider.enabled(false);
+  })
+  
+    .config(function ($routeProvider, ) {
         $routeProvider
             .when("/", {
                 templateUrl: "./views/login.html"
@@ -23,6 +32,7 @@ var DrinkApp = angular.module("DrinkApp", ['ngRoute'])
                 templateUrl: "./views/drinks.html"
             });
     })
+
     .run(function () {
         addb.init({
             appId: 14561
@@ -30,35 +40,79 @@ var DrinkApp = angular.module("DrinkApp", ['ngRoute'])
     })
 
 DrinkApp.controller('DrinksListController', function ($http, $scope, $routeParams, $location, $rootScope) {
-    addb.drinks('eg').skip(0).take(25).loadSet(function (query) {
+    var skip = 0
+    var take = 10
+    addb.drinks('eg').skip(skip).take(take).loadSet(function (query) {
         $scope.$apply(function () {
             $scope.drinks = query.result;
             console.log(query.result)
         });
+
+
     });
     $scope.getId = function (id) {
         $location.path('/drink/' + id)
     }
+    $scope.nextpage = function () {
+        skip += 10
+        take += 10
+        addb.drinks('eg').skip(skip).take(take).loadSet(function (query) {
+            $scope.$apply(function () {
+                $scope.drinks = query.result;
+                console.log(query.result)
+            });
+            window.scrollTo(0, 0);
+        })
+    }
+    $scope.prevpage = function () {
+        if (skip >= 10) {
+            skip -= 10
+            take -= 10
+            addb.drinks('eg').skip(skip).take(take).loadSet(function (query) {
+                $scope.$apply(function () {
+                    $scope.drinks = query.result;
+                    console.log(query.result)
+                });
+                window.scrollTo(0, 0);
+            })
+        }
+    }
 })
+
 
 DrinkApp.controller('SingleDrinkController', function ($http, $scope, $routeParams, $location, $rootScope) {
     var id = $routeParams.id
     console.log($routeParams)
     console.log(id)
-    addb.drinks().load( id , function(shake) {
+    addb.drinks().load(id, function (shake) {
         $scope.$apply(function () {
             $scope.singledrink = shake.result[0];
             console.log($scope.singledrink)
         });
     });
+    $scope.video = function (video) {
+        return 'http://www.youtube.com/embed/' + video
+        console.log(video(single.videos[0].video))
+    }
 })
 DrinkApp.controller('SearchController', function ($http, $scope, $routeParams, $location, $rootScope) {
-    var id = $routeParams.id
-
-    addb.drinks().quickSearch('some query', function(query) {
+    var search = document.getElementById('search').value
+    addb.drinks().quickSearch(search, function (query) {
         $scope.$apply(function () {
             $scope.search = query.result;
             console.log($scope.search)
         });
     });
 })
+
+// window.addEventListener('load', function() {
+//     document.querySelector('input[type="file"]').addEventListener('change', function() {
+//         if (this.files && this.files[0]) {
+//             var img = document.querySelector('img');  // $('img')[0]
+//             img.src = URL.createObjectURL(this.files[0]); // set src to file url
+//             img.onload = imageIsLoaded; // optional onload event listener
+//         }
+//     });
+//   });
+
+//   function imageIsLoaded(e) { alert(e); } 
