e125cde5f7f723e3978e9743da9a6e984af0e655
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-08-29 13:29:20 -0500

    single controller working

--- client/app.js
+++ client/app.js
@@ -1,41 +1,64 @@
 var DrinkApp = angular.module("DrinkApp", ['ngRoute'])
-.config(function ($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl: "./views/login.html"
-        })
-        .when("/home", {
-            templateUrl: "./views/home.html"
-        })
-        .when("/main", {
-            templateUrl: "./views/main.html"
-        })
-        .when("/user", {
-            templateUrl: "./views/user.html"
-        })
-        .when("/achievements", {
-            templateUrl: "./views/achievements.html"
-        })
-        .when("/drink/:id", {
-            templateUrl: "./views/singledrink.html"
-        })
-        .when("/drinks", {
-            templateUrl: "./views/drinks.html"
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "./views/login.html"
+            })
+            .when("/home", {
+                templateUrl: "./views/home.html"
+            })
+            .when("/main", {
+                templateUrl: "./views/main.html"
+            })
+            .when("/user", {
+                templateUrl: "./views/user.html"
+            })
+            .when("/achievements", {
+                templateUrl: "./views/achievements.html"
+            })
+            .when("/drink/:id", {
+                templateUrl: "./views/singledrink.html"
+            })
+            .when("/drinks", {
+                templateUrl: "./views/drinks.html"
+            });
+    })
+    .run(function () {
+        addb.init({
+            appId: 14561
         });
-})
-.run( function($rootScope){
-    $rootScope.drinks=[]
-    console.log(addb)
-    addb.init({
-        appId: 14561
+    })
+
+DrinkApp.controller('DrinksListController', function ($http, $scope, $routeParams, $location, $rootScope) {
+    addb.drinks('eg').skip(0).take(25).loadSet(function (query) {
+        $scope.$apply(function () {
+            $scope.drinks = query.result;
+            console.log(query.result)
+        });
     });
+    $scope.getId = function (id) {
+        $location.path('/drink/' + id)
+    }
 })
 
-DrinkApp.controller('DrinksListController', function($http, $scope, $routeParams, $location, $rootScope){
-addb.drinks().loadSet(function(query) { });
-addb.drinks('eg').skip(0).take(25).loadSet(function(query) { 
-    $scope.drinks = query.result
-    console.log($scope.drinks)
-    
-});
-}) 
+DrinkApp.controller('SingleDrinkController', function ($http, $scope, $routeParams, $location, $rootScope) {
+    var id = $routeParams.id
+    console.log($routeParams)
+    console.log(id)
+    addb.drinks().load( id , function(shake) {
+        $scope.$apply(function () {
+            $scope.singledrink = shake.result[0];
+            console.log($scope.singledrink)
+        });
+    });
+})
+DrinkApp.controller('SearchController', function ($http, $scope, $routeParams, $location, $rootScope) {
+    var id = $routeParams.id
+
+    addb.drinks().quickSearch('some query', function(query) {
+        $scope.$apply(function () {
+            $scope.search = query.result;
+            console.log($scope.search)
+        });
+    });
+})
