daa06ecee7829b245265edc975f70a0b2a86d0be
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-08-30 15:08:13 -0500

    search sort of works...

--- app.js
+++ app.js
@@ -94,56 +94,98 @@ DrinkApp.controller('SingleDrinkController', function ($http, $scope, $routePara
         console.log(video(single.videos[0].video))
     }
 })
+
 DrinkApp.controller('SearchController', function ($http, $scope, $routeParams, $location, $rootScope) {
-    console.log('in search controller')
     $scope.locate = function () {
-        console.log('search working')
         var input = document.getElementById('usersearch').value
         console.log(input)
-        addb.drinks().tasting(input).loadSet(function (query) {
-            $scope.$apply(function () {
-                if (query.result.length != 0) {
-                    $rootScope.$apply.searchresults.push(query.result)
-                }
-            });
-        });
-        addb.drinks().withIngredient(input).loadSet(function (query) {
-            $scope.$apply(function () {
-                if (query.result.length != 0) {
-                    $rootScope.$apply.searchresults.push(query.result)
-                }
-            });
-        })
-        addb.drinks().load(input, function (shake) {
-            $scope.$apply(function () {
-                if (shake.result.length != 0) {
-                    $rootScope.$apply.searchresults.push(shake.result)
-                }
-            });
-        })
-        if ($rootScope.$apply.searchresults = []) {
-            alert('Your search ' + '"' + input + '"' + ' returned no results! Please check your spelling and try again.')
-        }
-
-
-        // addb.drinks().typeIngredient(input).loadSet(function(query) {
+        // addb.drinks().tasting(input).loadSet(function (query) {
         //     $scope.$apply(function () {
-        //         $scope.ingredient = query.result;
-        //         console.log($scope.ingredient)
-        //         if(query.result.length != 0){
+        //         if (query.result.length != 0) {
+        //             console.log(query.result)
+        //             // var parse1= JSON.parse(query.result[0])
         //             $rootScope.$apply.searchresults.push(query.result)
         //         }
-
-
+        //     });
+        // });
+        // addb.drinks().withIngredient(input).loadSet(function (query) {
+        //     $scope.$apply(function () {
+        //         // $scope.ingredient = query.result;
+        //         // console.log($scope.ingredient)
+        //         if (query.result.length != 0) {
+        //             $rootScope.$apply.searchresults.push(query.result)
+        //         }
+        //     });
+        // })
+        // addb.drinks().load(input, function (shake) {
+        //     $scope.$apply(function () {
+        //         if (shake.result.length != 0) {
+        //             $rootScope.$apply.searchresults.push(shake.result)
+        //         }
         //     });
         // })
 
-        $location.path('/searchresult/' + input)
-        console.log($rootScope.$apply.searchresults)
+        var options = {
+            type: $scope.type,
+            ingredient: $scope.ingredient
+        }
+
+        myFactory.getDrinks(options)
+
+         addb.drinks('eg').skip(0).take(300).loadSet(function (query) {
+            $scope.$apply(function () {
+                var type = 'whisky';
+                var filtered = [];
+
+               var results = query.result;
+
+               results.forEach((r) => {
+                    r.ingredients.forEach((i) => {
+                        if (i.type === type) {
+                            filtered.push(r);
+                        }
+                    });
+               });
+
+               console.log(filtered);
+            //     temparray=[]
+            // if(query.result){
+            //     for(x=0; x< query.result.length; x++){
+            //             for(y=0; y<query.result[x].ingredients.length; y++){
+            //                 if(input== query.result[x].ingredients[y].type ){
+            //                     // console.log(query.result[x])
+            //                         temparray.push(query.result[x])
+            //                 }
+            //             }
+            //     }
+            // }
+            // $rootScope.$apply.searchresults.push(temparray)
+            });
+        })
+
+        if ($rootScope.$apply.searchresults = []) {
+                alert('Your search '+ '"' +input+ '"' + ' returned no results! Please check your spelling and try again.')
+            }
+    
+
+// addb.drinks().typeIngredient(input).loadSet(function(query) {
+//     $scope.$apply(function () {
+//         $scope.ingredient = query.result;
+//         console.log($scope.ingredient)
+//         if(query.result.length != 0){
+//             $rootScope.$apply.searchresults.push(query.result)
+//         }
+
+
+//     });
+// })
+console.log($rootScope.$apply.searchresults)
+$location.path('/searchresult/' + input)
+
 
     }
-
+    
 })
-addtofavorites = function () {
+addtofavorites= function (){
 
 }
